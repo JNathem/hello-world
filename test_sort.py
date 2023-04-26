2c80fa9cdbaf800336d4263c12eb80ac43ded148
JNathem <post@nathem.info>
2023-04-26 23:42:39 +0200

    Test Sort implementation

---
+++ test_sort.py
@@ -0,0 +1,13 @@
+prime_nr = [97,83,2,3,11,7]
+
+# Sort the list
+prime_nr.sort()
+print(prime_nr)
+
+# Reverse the list
+prime_nr.reverse()
+print(prime_nr)
+
+# Sort the list in reverse order
+prime_nr.sort(reverse=True)
+print(prime_nr)
