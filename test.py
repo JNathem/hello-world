6258090b5e5c79c2c6f8fe222b716a67108e35dd
JNathem <post@nathem.info>
2023-04-26 13:38:04 +0200

    Update for testing puposes

---
+++ test.py
@@ -0,0 +1,16 @@
+Test = ["Test1","Test2","Test3"]
+
+Vari = Test[0]
+
+Test[0]="Test_neu"
+
+print(Vari)
+print(Test[0])
+
+for i in range(0,len(Test)):
+    print(Test[i])
+
+Test[0] = "Test1"
+
+for i in range(0,len(Test)):
+    print(Test[i])
